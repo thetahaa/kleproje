bb11e30dc094c3b66a17e1440e4e7e49076e482d
Taha <tahakarakaya777@gmail.com>
2025-04-11 00:32:09 +0300

    frontend

--- klefrontend/resources/views/product/index.blade.php
+++ klefrontend/resources/views/product/index.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="{{ asset('css/product.css') }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="{{ asset('images/panther.png') }}">
     <title>kle | Ürünler</title>
 </head>
 
